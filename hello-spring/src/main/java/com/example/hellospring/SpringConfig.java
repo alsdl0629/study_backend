d8e0a82c3cf5a93898953b4ce8613de791c90ab2
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-06-21 09:27:42 +0900

    ADD::JdbcTempateMemberRepository

--- hello-spring/src/main/java/com/example/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/com/example/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package com.example.hellospring;
 
+import com.example.hellospring.repository.JdbcTemplateMemberRepository;
 import com.example.hellospring.repository.MemberRepository;
-import com.example.hellospring.repository.MemoryMemberRepository;
 import com.example.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean//스프링빈 등록
     public MemberService memberService() {
@@ -16,7 +26,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository(); //인터페이스는 new 안됨
+//        return new MemoryMemberRepository(); //인터페이스는 new 안됨
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
